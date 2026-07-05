5748069f83f3d1354c167fe1ca3301ff3c7d43cd
ansarifarhan9542 <fa9542647@gmail.com>
2026-07-05 22:05:26 +0530

    Create Buttons.cpp

---
+++ 800_rated_questions/Buttons.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  int t;
+  cin>>t;
+  while(t--){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b || (a==b && c%2==1)) cout<<first<<endl;
+    else if(b>a || (a==b && c%2==0)) cout<<second<<endl;
+  }
+  return 0;
+}
